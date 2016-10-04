c53bbffc41c0bd17fceba056edef9a19ad40215c
anknits <ankuragarwall04@gmail.com>
2016-10-04 11:38:04 +0000

    second update

--- server.js
+++ server.js
@@ -1,1 +1,70 @@
-var express=require("express")
+var express = require('express');
+var app = express();
+var mongodb = require('mongodb');
+var validUrl = require('valid-url');
+var MongoClient = mongodb.MongoClient;
+var mongourl = process.env.mongolab_url
+var input = ''
+var hosturl = 'https://apidevelopmentprojects-anknits.c9users.io'
+
+function getMaxId(db){
+  var id = db.collection('urlcoll').find({},{_id : 1}).sort({_id : -1}).limit(1)
+  return id[0]
+}
+
+function dbOperations(redirect){
+MongoClient.connect(mongourl, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } 
+  else {
+    console.log('Connection established to', mongourl);
+    var coll = db.collection('urlcoll')
+    if (redirect !=1)
+    {
+        coll.find({originalurl: input}).limit(1).toArray(function(error,documents){
+        if (error) throw error
+        if(documents.length == 1)
+           return documents[0]._id
+        else
+        {
+          var ret = coll.insertOne({_id : getMaxId(db)+1,originalurl : input})
+          return ret.insertedId
+        }
+    })
+    }
+    else{
+      var doc = coll.findAndModify({ query: {_id : input}, update: { $inc: { hits: 1 } }, fields: {originalurl : 1}})
+      return doc[0].originalurl
+    }
+    db.close();
+  }
+});
+}
+
+app.get('/new/:url*', function (req, res) {
+    input = req.params.url
+    if (validUrl.isUri(input)){
+      var id = dbOperations(0)
+      var shorturl = hosturl+"/"+id
+      res.send(JSON.stringify({"original_url: " : input , "short_url: " : shorturl}));  
+    } else {
+        res.send(JSON.stringify({" the url you passed seems invalid: " : input}))
+    }
+    
+});
+
+app.get('/:id', function(req,res){
+  input = req.params.id
+  if (validUrl.isUri(input)){
+    var originalurl = dbOperations(1)
+    res.redirect(originalurl)
+  }
+  else {
+    res.send(JSON.stringify({" the url you passed seems invalid: " : input}))
+    }
+})
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port', process.env.PORT);
+});
